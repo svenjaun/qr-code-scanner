4eef087d61e6429c1a151a88da8fcdc932566c33
svenjaun <sjaun@gmx.ch>
2019-03-25 17:21:09 +0100

    Generate QR-Codes Images

--- src/app/src/main/java/com/example/qr_code_scanner/Fragments/DetailFragment.java
+++ src/app/src/main/java/com/example/qr_code_scanner/Fragments/DetailFragment.java
@@ -1,24 +1,50 @@
 package com.example.qr_code_scanner.Fragments;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.RequiresApi;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.qr_code_scanner.R;
+import com.example.qr_code_scanner.database.QRCodeData;
+import com.example.qr_code_scanner.database.datatypes.QRCodeModel;
+import com.google.zxing.WriterException;
+
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 
 
 public class DetailFragment extends Fragment {
 
+    ImageView detailQRCode;
+    QRCodeModel qrCode;
+    QRCodeData qrcodeData;
+    int qrCodeId;
+
     public DetailFragment() {
         // Required empty public constructor
+        qrCodeId = 1;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        qrcodeData = new QRCodeData(getContext());
+        qrCode = qrcodeData.getQRCode(qrCodeId);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        detailQRCode = view.findViewById(R.id.detail_fragment_qr_code);
+        detailQRCode.setImageBitmap(setQRCode(qrCode.getValue()));
+        super.onViewCreated(view, savedInstanceState);
     }
 
     @Override
@@ -36,5 +62,17 @@ public class DetailFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
+    }
+
+    private Bitmap setQRCode(String value) {
+        Bitmap qrcode;
+        QRGEncoder qrgEncoder = new QRGEncoder(value, null, QRGContents.Type.TEXT, 200);
+        try {
+            // Getting QR-Code as Bitmap
+            qrcode = qrgEncoder.encodeAsBitmap();
+        } catch (WriterException e) {
+            throw new Error("---------------------------------------------Error by generation QRCode: " + e);
+        }
+        return qrcode;
     }
 }
--- src/app/src/main/java/com/example/qr_code_scanner/QrCodeListAdapter.java
+++ src/app/src/main/java/com/example/qr_code_scanner/QrCodeListAdapter.java
@@ -1,28 +1,31 @@
 package com.example.qr_code_scanner;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.example.qr_code_scanner.Fragments.DetailFragment;
 import com.example.qr_code_scanner.database.datatypes.QRCodeModel;
+import com.google.zxing.WriterException;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Locale;
 
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
+
 public class QrCodeListAdapter extends RecyclerView.Adapter<ViewHolder> {
     @NonNull
     private Activity activity;
     @NonNull
     private ArrayList<QRCodeModel> qrCodes;
-
-    TextView qrcodeDataTV;
 
     public QrCodeListAdapter(@NonNull Activity activity, @NonNull ArrayList<QRCodeModel> qrCodes) {
         this.activity = activity;
@@ -33,7 +36,6 @@ public class QrCodeListAdapter extends RecyclerView.Adapter<ViewHolder> {
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View v = LayoutInflater.from(activity).inflate(R.layout.list_element, viewGroup, false);
-        qrcodeDataTV = activity.findViewById(R.id.list_element_qr_code_date);
         ViewHolder viewHolder = new ViewHolder(activity, v);
 
 
@@ -63,8 +65,20 @@ public class QrCodeListAdapter extends RecyclerView.Adapter<ViewHolder> {
         Calendar cal = qrCode.getDate();
         cal.add(Calendar.DATE, 1);
         SimpleDateFormat format = new SimpleDateFormat("EEEE dd.MMMM yyyy", Locale.GERMAN);
-
         viewHolder.getListEllementDateTextView().setText(format.format(cal.getTime()));
+        viewHolder.getListElementQrCode().setImageBitmap(setQRCode(qrCode.getValue()));
+    }
+
+    private Bitmap setQRCode(String value) {
+        Bitmap qrcode;
+        QRGEncoder qrgEncoder = new QRGEncoder(value, null, QRGContents.Type.TEXT, 100);
+        try {
+            // Getting QR-Code as Bitmap
+            qrcode = qrgEncoder.encodeAsBitmap();
+        } catch (WriterException e) {
+            throw new Error("Error by generation QRCode: " + e);
+        }
+        return qrcode;
     }
 
     @NonNull
